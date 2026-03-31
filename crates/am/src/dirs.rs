a52431182add8c58f52e65fbe438908ba74c154c
Sven Kanoldt <sven@d34dl0ck.me>
2026-03-31 11:46:29 +0200

    fix: fix the config dir to be .config/amoxide/
    Signed-off-by: Sven Kanoldt <sven@d34dl0ck.me>

--- crates/am/src/dirs.rs
+++ crates/am/src/dirs.rs
@@ -2,9 +2,9 @@ use std::path::PathBuf;
 
 /// Returns the path to the config directory
 ///
-/// Located at `$HOME/.config/alias-manager`
+/// Located at `$HOME/.config/amoxide`
 pub fn config_dir() -> PathBuf {
-    dirs_lite::config_dir().unwrap().join("alias-manager")
+    dirs_lite::config_dir().unwrap().join("amoxide")
 }
 
 /// Returns the user's home directory.
